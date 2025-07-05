c2ce2f629fe75219970084dd93e32e1342be3c07
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-07-05 11:27:32 -0300

    configura projeto para usar ssr

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here excluir linha debaixo para usar ssr*/ 
-  output: 'export',
+  /* config options here comentar linha debaixo para usar ssr*/ 
+  // output: 'export',
 };
 
 export default nextConfig;
